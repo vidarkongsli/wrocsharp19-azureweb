104401d78f232980dabaf571237e5f20db38cc66
Vidar Kongsli <vidar@kongs.li>
2019-03-12 22:45:36 +0100

    Remove build files

--- build.ps1
+++
@@ -1,3 +0,0 @@
-& $PSScriptRoot\deploy_custom.ps1 `
-    -deploymentDirectory "$($PSScriptRoot)\dist" `
-    -deploymentTemp "$env:temp\___deployTemp$(-join ((48..57) * 32 | Get-Random -Count 32 | ForEach-Object {[char]$_}))" `
